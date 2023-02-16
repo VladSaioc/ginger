05e511a98eafd50eaaec206c4cf199d84f858572
Vlad Saioc <vladsaioc10@gmail.com>
2023-02-16 15:23:56 +0000

    Promela AST refinement.

---
+++ src/Promela/Ast.hs
@@ -0,0 +1,71 @@
+module Promela.Ast where
+
+import Data.List
+import Utilities.Position
+
+type Ident = String
+
+data Spec = Spec [Module] deriving (Eq, Ord, Read, Show)
+
+data Module = TopDecl Ident Type Val
+  | Init [Pos Stmt]
+  | Proc Ident [Param] [Pos Stmt]
+  | Typedef Ident [Field]
+  deriving (Eq, Ord, Read, Show)
+
+type Field = (Ident, Type, Maybe Exp)
+
+type Param = (Ident, Type)
+
+data Stmt = Decl Ident Type (Maybe Exp)
+  | If [[Pos Stmt]] (Maybe [Pos Stmt])
+  | Do [[Pos Stmt]] (Maybe [Pos Stmt])
+  | For Range [Pos Stmt]
+  | As LVal Exp
+  | Goto Ident
+  | Break
+  | Skip
+  | Assert Exp
+  | Rcv LVal [Exp]
+  | Send LVal [Exp]
+  | ExpS Exp
+  | Label Ident (Pos Stmt)
+  deriving (Eq, Ord, Read, Show)
+
+data Exp = Chan Integer
+  | Const Val
+  | And Exp Exp
+  | Or Exp Exp
+  | Eq Exp Exp
+  | Ne Exp Exp
+  | Le Exp Exp
+  | Lt Exp Exp
+  | Ge Exp Exp
+  | Gt Exp Exp
+  | Plus Exp Exp
+  | Minus Exp Exp
+  | Mult Exp Exp
+  | Div Exp Exp
+  | Neg Exp
+  | Not Exp
+  | Len LVal
+  | EVar LVal
+  | Run Ident [Exp]
+  deriving (Eq, Ord, Read, Show)
+
+data Range = Between Ident Exp Exp
+  | In Ident Ident
+  deriving (Eq, Ord, Read, Show)
+
+data LVal = Var Ident
+  | Field LVal Ident
+  | Arr LVal Exp
+  deriving (Eq, Ord, Read, Show)
+
+data Val = VInt Integer | VBool Bool deriving (Eq, Ord, Read, Show)
+
+data Type = TInt
+  | TBool
+  | TChan
+  | TNamed Ident
+  deriving (Eq, Ord, Read, Show)
