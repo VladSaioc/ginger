6a37fe8b46caa3880393f575d932789ed7d3587d
Vlad Saioc <vladsaioc10@gmail.com>
2023-04-13 09:43:06 +0100

    Removed Utitlites/General.hs warnings.

--- src/Utilities/General.hs
+++ src/Utilities/General.hs
@@ -1,28 +1,19 @@
 module Utilities.General (results, binaryCons, unaryCons) where
 
-import Control.Monad
-
-import Utilities.Err
-import Utilities.Position
-
-errMsg :: String -> Integer -> String -> Err alpha
-errMsg file row msg = Bad (file ++ ":" ++ show row ++ ": " ++ msg)
-
-results :: Monad m => Eq alpha => [m alpha] -> m [alpha]
+results :: Monad m => Eq a => [m a] -> m [a]
 results [] = return []
-results (r:rs) = do
+results (r : rs) = do
   rv <- r
-  rs <- results rs
-  return (rv : rs)
+  rs' <- results rs
+  return (rv : rs')
 
 binaryCons :: Monad m => (a -> m b) -> (b -> b -> c) -> a -> a -> m c
 binaryCons p cons e1 e2 = do
-  e1 <- p e1
-  e2 <- p e2
-  return (cons e1 e2)
-
+  e1' <- p e1
+  e2' <- p e2
+  return (cons e1' e2')
 
 unaryCons :: Monad m => (a -> m b) -> (b -> c) -> a -> m c
 unaryCons p cons e = do
-  e <- p e
-  return (cons e)
+  e' <- p e
+  return (cons e')
