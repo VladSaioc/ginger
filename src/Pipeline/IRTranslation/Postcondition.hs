4c72e505cb3d6bc4b594c18744a7345c397c4ce0
Vlad Saioc <vladsaioc10@gmail.com>
2023-04-21 11:33:26 +0100

    Postcondition synthesis.

---
+++ src/Pipeline/IRTranslation/Postcondition.hs
@@ -0,0 +1,18 @@
+module Pipeline.IRTranslation.Postcondition (postconditions) where
+
+import Backend.Ast
+import Backend.Utilities
+import Data.Map qualified as M
+import Pipeline.IRTranslation.Utilities
+
+postconditions :: Procs -> [Exp]
+postconditions = map (uncurry postcondition) . M.toList
+
+{- Constructs a postcondition for the given process.
+Depends on: π, ϕ
+
+Produces:
+pc(π) = (max ∘ dom)(ϕ)
+-}
+postcondition :: Pid -> ProgPoints -> Exp
+postcondition pid pp = Eq ((pid <|) @) ((pp -|) #)
