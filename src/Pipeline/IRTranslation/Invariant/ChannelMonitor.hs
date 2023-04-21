cb1049cc90c2a8f3a78a5a74b68ff261532a96e8
Vlad Saioc <vladsaioc10@gmail.com>
2023-04-21 11:56:45 +0100

    Channel monitor invariant synthesis.

---
+++ src/Pipeline/IRTranslation/Invariant/ChannelMonitor.hs
@@ -0,0 +1,60 @@
+module Pipeline.IRTranslation.Invariant.ChannelMonitor (channelMonitors) where
+
+import Backend.Ast
+import Backend.Utilities
+import Data.List qualified as L
+import Data.Map qualified as M
+import Data.Maybe qualified as Mb
+import Data.Set qualified as S
+import IR.Utilities
+import Pipeline.IRTranslation.Utilities
+
+channelMonitors :: PChInsns -> [Loop] -> [Exp]
+channelMonitors noloopOps ls =
+  let noloopSubexps = L.map snd (M.toList (M.mapWithKey noloopMonitors noloopOps))
+      loopSubexps = L.map loopMonitor ls
+      subexps = M.unionsWith (M.unionWith Plus) (noloopSubexps ++ loopSubexps)
+      chanMonitor c dir =
+        let sendops = Mb.fromMaybe (0 #) (M.lookup S dir)
+            recvops = Mb.fromMaybe (0 #) (M.lookup R dir)
+         in Eq (c @) (Minus sendops recvops)
+   in M.elems (M.mapWithKey chanMonitor subexps)
+
+{- Monitor channel value by analyzing a loop
+Depends on:
+-}
+loopMonitor :: Loop -> ChMap (M.Map OpDir Exp)
+loopMonitor (Loop {pid, var, exitP, chans}) =
+  let x = (var @)
+      pc = ((pid <|) @)
+      singleOp op =
+        let hasPassedOp = And (Lt (op #) pc) (Lt pc (exitP #))
+         in IfElse hasPassedOp (1 #) (0 #)
+      chanSubexp ops =
+        let iterations = Mult x (S.size ops #)
+            ops' = L.map singleOp (S.toList ops)
+         in Plus iterations (ops' .+.)
+   in M.map (M.map chanSubexp) chans
+
+{- Organize and compose under addition all non-loop monitor
+sub-expressions for every channel for a given process.
+Depends on: π,
+
+-}
+noloopMonitors :: Pid -> ChMap ChOps -> ChMap (M.Map OpDir Exp)
+noloopMonitors pid =
+  let pc = ((pid <|) @)
+      subexps = L.map (noloopMonitor pc) . S.toList
+      setTransform = (.+.) . subexps
+   in M.map (M.map setTransform)
+
+{- Monitor sub-expression for a non-loop single channel operation. If the
+operation has occurred, its resource contribution is 1.
+Depends on: π, n, where n ∈ dom(Π(π))
+
+if n < pc(π) then 1 else 0
+-}
+noloopMonitor :: Exp -> PCounter -> Exp
+noloopMonitor pc n =
+  let passed = Lt (n #) pc
+   in IfElse passed (1 #) (0 #)
