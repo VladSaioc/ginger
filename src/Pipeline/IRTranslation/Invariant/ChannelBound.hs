9cbd86ec6214b2429a5f0a53fa3e8ea77286d8b1
Vlad Saioc <vladsaioc10@gmail.com>
2023-04-21 11:29:39 +0100

    Comments for channel bound invariant synthesis.

--- src/Pipeline/IRTranslation/Invariant/ChannelBound.hs
+++ src/Pipeline/IRTranslation/Invariant/ChannelBound.hs
@@ -1,16 +1,23 @@
-module Pipeline.IRTranslation.Invariant.ChannelBound (buildChBoundInvs) where
+module Pipeline.IRTranslation.Invariant.ChannelBound (channelBounds) where
 
 import Backend.Ast
+import Backend.Utilities
 import Data.Map qualified as M
 import Pipeline.IRTranslation.Utilities
 
-buildChBoundInvs :: KEnv -> [Exp]
-buildChBoundInvs = map buildChBoundInv . M.toList
+{- Composes all channel bound invariants.
+-}
+channelBounds :: KEnv -> [Exp]
+channelBounds = map channelBound . M.toList
 
-buildChBoundInv :: (String, Exp) -> Exp
-buildChBoundInv (c, e) =
-  let c' = EVar c
-      zero = ECon (CNum 0)
-      lower = Leq zero c'
-      upper = Leq c' e
+{- Constructs a channel bound invariant.
+Depends on: κ, c
+
+Produces:
+0 ≤ c ∧ c ≤ κ(c)
+-}
+channelBound :: (String, Exp) -> Exp
+channelBound (c, e) =
+  let lower = Leq (0 #) (c @)
+      upper = Leq (c @) e
    in And lower upper
