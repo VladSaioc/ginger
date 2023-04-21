ae8e0fbfe29f3e1ebf9388bdceaf41d6099ce531
Vlad Saioc <vladsaioc10@gmail.com>
2023-04-21 10:10:47 +0100

    IR syntax utilities module.

---
+++ src/IR/Utilities.hs
@@ -0,0 +1,15 @@
+module IR.Utilities where
+
+import IR.Ast
+
+data OpDir = S | R deriving (Eq, Ord, Read)
+
+chName :: Op -> String
+chName = \case
+  Send c -> c
+  Recv c -> c
+
+chDir :: Op -> OpDir
+chDir = \case
+  Send _ -> S
+  Recv _ -> R
