13a2201c3475567a6df4bf25244be3c421368ac9
Vlad Saioc <vladsaioc10@gmail.com>
2023-05-01 19:49:48 +0100

    IR implements Show.

--- src/IR/Ast.hs
+++ src/IR/Ast.hs
@@ -38,3 +38,41 @@ data Exp
   | -- x
     Var String
   deriving (Eq, Ord, Read)
+
+instance Show Prog where
+  show (Prog cs ps) =
+    let showp s = unlines ["go {", show s, "}"]
+        cs' = unlines (map show cs)
+        ps' = unlines (map showp ps)
+     in concat [cs', "\n", ps']
+
+instance Show Chan where
+  show (Chan c e) = unwords [c, "=", "[" ++ show e ++ "];"]
+
+instance Show Stmt where
+  show = \case
+    Seq s1 s2 -> unlines ["  " ++ show s1 ++ ";", "  " ++ show s2]
+    Skip -> "skip"
+    Atomic o -> "  " ++ show o
+    For x e1 e2 os ->
+      unlines
+        ( [unwords ["for", x, ":", show e1, "..", show e2, "{"]]
+            ++ map (\o -> ("    " ++ show o ++ ";")) os
+        )
+        ++ "  }"
+
+instance Show Exp where
+  show =
+    let bin e1 op e2 = unwords ["(" ++ show e1 ++ ")", op, "(" ++ show e2 ++ ")"]
+     in \case
+          Plus e1 e2 -> bin e1 "+" e2
+          Minus e1 e2 -> bin e1 "-" e2
+          Mult e1 e2 -> bin e1 "*" e2
+          Div e1 e2 -> bin e1 "/" e2
+          Const n -> show n
+          Var x -> x
+
+instance Show Op where
+  show = \case
+    Send c -> c ++ "!"
+    Recv c -> c ++ "?"
