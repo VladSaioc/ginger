137a6186b1f59ace78135daf2530dc23b88fdc88
Vlad Saioc <vladsaioc10@gmail.com>
2023-05-01 13:21:13 +0100

    Monad fold.

--- src/Utilities/General.hs
+++ src/Utilities/General.hs
@@ -1,11 +1,21 @@
-module Utilities.General (results, binaryCons, unaryCons) where
+module Utilities.General (results, foldMonad, binaryCons, unaryCons) where
 
-results :: Monad m => Eq a => [m a] -> m [a]
+results :: Monad m => [m a] -> m [a]
 results [] = return []
 results (r : rs) = do
   rv <- r
   rs' <- results rs
   return (rv : rs')
+
+foldMonad :: (Foldable t1, Monad m) => (t2 -> m t3) -> t4 -> (t4 -> t3 -> t4) -> t1 t2 -> m t4
+foldMonad process start combine =
+  Prelude.foldl
+    ( \ms e -> do
+        s1 <- ms
+        s2 <- process e
+        return $ combine s1 s2
+    )
+    (return start)
 
 binaryCons :: Monad m => (a -> m b) -> (b -> b -> c) -> a -> a -> m c
 binaryCons p cons e1 e2 = do
