f3304bf9a1b422d45e9ecc50712b2840ee11eb0f
Vlad Saioc <vladsaioc10@gmail.com>
2023-07-12 15:23:06 +0200

    Minor refactoring in capacity preconditions.

--- src/Pipeline/IRTranslation/CapPrecondition.hs
+++ src/Pipeline/IRTranslation/CapPrecondition.hs
@@ -6,13 +6,9 @@ import Data.List qualified as L
 import Data.Map qualified as M
 import Pipeline.IRTranslation.Utilities
 
--- loopPreconditions :: Procs -> [Exp]
-
--- loopPreconditions pp
-
 -- Aggregate all asynchrony preconditions.
 capPreconditions :: KEnv -> [Exp]
-capPreconditions = L.map asyncPrecondition . M.elems
+capPreconditions = L.map capPrecondition . M.elems
 
 {- Constructs a precondition certifying that the channel capacity
 is a valid expression i.e., a positive integer.
@@ -21,5 +17,5 @@ Depends on: κ, c
 Produces:
 κ(c) ≥ 0
 -}
-asyncPrecondition :: Exp -> Exp
-asyncPrecondition = Leq (0 #)
+capPrecondition :: Exp -> Exp
+capPrecondition = Leq (0 #)
