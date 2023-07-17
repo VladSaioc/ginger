2c4813b99dea747e990d60446823669c3e36f766
Vlad Saioc <vladsaioc10@gmail.com>
2023-07-17 08:47:16 +0200

    Channel bounds reworked to account for unbuffered channels.

--- src/Pipeline/IRTranslation/Invariant/ChannelBound.hs
+++ src/Pipeline/IRTranslation/Invariant/ChannelBound.hs
@@ -6,18 +6,34 @@ import Data.Map qualified as M
 import Pipeline.IRTranslation.Utilities
 
 {- Composes all channel bound invariants.
--}
-channelBounds :: KEnv -> [Exp]
-channelBounds = map channelBound . M.toList
-
-{- Constructs a channel bound invariant.
-Depends on: κ, c
+Depends on: κ
 
 Produces:
-0 ≤ c ∧ c ≤ κ(c)
+∀ c ∈ dom(κ). channelBound(c, κ(c))
 -}
-channelBound :: (String, Exp) -> Exp
-channelBound (c, e) =
-  let lower = Leq (0 #) (c @)
-      upper = Leq (c @) e
-   in And lower upper
+channelBounds :: KEnv -> [Exp]
+channelBounds = map (uncurry channelBound) . M.toList
+
+{- Constructs a channel bound invariant.
+Depends on: c, κ(c)
+
+Produces:
+(κ(c) > 0 ⇒ 0 ≤ c ∧ c ≤ κ(c)) ∧ (κ(c) = 0 ⇒ c ∈ {1, 0, -1})
+-}
+channelBound :: String -> Exp -> Exp
+channelBound c k =
+  let -- 0 ≤ c
+      lower = Leq (0 #) (c @)
+      -- c ≤ κ(c)
+      upper = Leq (c @) k
+      -- κ(c) = 0
+      isSync = Eq k (0 #)
+      -- κ(c) ≥ 0
+      isAsync = Gt k (0 #)
+      -- 0 ≤ c ∧ c ≤ κ(c)
+      asyncBound = And lower upper
+      -- c ∈ {1, 0, -1}
+      syncBound = In (c @) (ESet [(1 #), (0 #), ((-1) #)])
+   in And
+        (Implies isSync syncBound)
+        (Implies isAsync asyncBound)
