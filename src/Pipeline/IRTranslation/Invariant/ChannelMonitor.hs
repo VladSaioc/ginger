6ac7653119936e3f20d91a440d35f9496f1232dc
Vlad Saioc <vladsaioc10@gmail.com>
2023-07-17 14:52:25 +0200

    General channel monitor composes sync and async behaviour.

--- src/Pipeline/IRTranslation/Invariant/ChannelMonitor.hs
+++ src/Pipeline/IRTranslation/Invariant/ChannelMonitor.hs
@@ -2,59 +2,34 @@ module Pipeline.IRTranslation.Invariant.ChannelMonitor (channelMonitors) where
 
 import Backend.Ast
 import Backend.Utilities
-import Data.List qualified as L
 import Data.Map qualified as M
 import Data.Maybe qualified as Mb
-import Data.Set qualified as S
-import IR.Utilities
+import Pipeline.IRTranslation.Invariant.ChannelAsyncMonitor (asyncChannelMonitors)
+import Pipeline.IRTranslation.Invariant.ChannelSyncMonitor (syncChannelMonitors)
 import Pipeline.IRTranslation.Utilities
 
-channelMonitors :: PChInsns -> [Loop] -> [Exp]
-channelMonitors noloopOps ls =
-  let noloopSubexps = L.map snd (M.toList (M.mapWithKey noloopMonitors noloopOps))
-      loopSubexps = L.map loopMonitor ls
-      subexps = M.unionsWith (M.unionWith Plus) (noloopSubexps ++ loopSubexps)
-      chanMonitor c dir =
-        let sendops = Mb.fromMaybe (0 #) (M.lookup S dir)
-            recvops = Mb.fromMaybe (0 #) (M.lookup R dir)
-         in Eq (c @) (Minus sendops recvops)
-   in M.elems (M.mapWithKey chanMonitor subexps)
+{- Produces channel monitors by composing both buffered and
+unbuffered channel behaviour under conjoined implications.
 
-{- Monitor channel value by analyzing a loop
 Depends on:
+1. All program loops: [ℓ]
+2. All non-loop operations:
+    O = {(π, n, o) | (n, o) ∉ op(ℓ), ℓ ∈ [ℓ], (n, o) ∈ ϕ, (π, ϕ) ∈ Π }
+3. Channel capacity environments: κ
+
+∀ c, e1 = syncChannelMonitor(O, [ℓ])(c),
+     e2 = asyncChannelMonitor(O, [ℓ])(c).
+    (κ(c) = 0 ⇒ c = e1)
+  ∧ (κ(c) > 0 ⇒ c = e2)
 -}
-loopMonitor :: Loop -> ChMap (M.Map OpDir Exp)
-loopMonitor (Loop {pid, var, lower, exitP, chans}) =
-  let x = (var @)
-      pc = ((pid <|) @)
-      singleOp op =
-        let hasPassedOp = And (Lt (op #) pc) (Lt pc (exitP #))
-         in IfElse hasPassedOp (1 #) (0 #)
-      chanSubexp ops =
-        let iterations = Mult (Minus x lower) (S.size ops #)
-            ops' = L.map singleOp (S.toList ops)
-         in Plus iterations (ops' .+.)
-   in M.map (M.map chanSubexp) chans
-
-{- Organize and compose under addition all non-loop monitor
-sub-expressions for every channel for a given process.
-Depends on: π,
-
--}
-noloopMonitors :: Pid -> ChMap ChOps -> ChMap (M.Map OpDir Exp)
-noloopMonitors pid =
-  let pc = ((pid <|) @)
-      subexps = L.map (noloopMonitor pc) . S.toList
-      setTransform = (.+.) . subexps
-   in M.map (M.map setTransform)
-
-{- Monitor sub-expression for a non-loop single channel operation. If the
-operation has occurred, its resource contribution is 1.
-Depends on: π, n, where n ∈ dom(Π(π))
-
-if n < pc(π) then 1 else 0
--}
-noloopMonitor :: Exp -> PCounter -> Exp
-noloopMonitor pc n =
-  let passed = Lt (n #) pc
-   in IfElse passed (1 #) (0 #)
+channelMonitors :: KEnv -> PChInsns -> [Loop] -> [Exp]
+channelMonitors kenv noloopOps ls =
+  let syncMs = syncChannelMonitors noloopOps ls
+      asyncMs = asyncChannelMonitors noloopOps ls
+      combineMonitors c s a =
+        let cap = Mb.fromMaybe (0 #) (M.lookup c kenv)
+            isSync = Eq (0 #) cap
+            isAsync = Lt (0 #) cap
+            eqc = Eq (c @)
+         in And (Implies isSync $ eqc s) (Implies isAsync $ eqc a)
+   in M.elems $ M.unionWithKey combineMonitors syncMs asyncMs
