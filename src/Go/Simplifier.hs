93a9bbfcb3a68a74f6d713b9fcf68a6e33c98d01
Vlad Saioc <vladsaioc10@gmail.com>
2023-07-25 13:07:21 +0200

    Commented out Go simplifier.

--- src/Go/Simplifier.hs
+++ src/Go/Simplifier.hs
@@ -2,25 +2,25 @@ module Go.Simplifier where
 
 import Go.Ast
 
-simplify :: Prog -> Prog
-simplify p@(Prog ss) = 
-  let simpl = Prog $ simplifyStatements ss
-    in if simpl == p
-      then simpl
-      else simplify simpl
+-- simplify :: Prog -> Prog
+-- simplify p@(Prog ss) =
+--   let simpl = Prog $ simplifyStatements ss
+--    in if simpl == p
+--         then simpl
+--         else simplify simpl
 
-simplifyStatements :: [Stmt] -> [Stmt]
-simplifyStatements = \case
-  [] -> []
-  s : ss -> case s of
-      Skip -> simplifyStatements ss
-      Return -> [Return]
-      Break  -> [Break]
-      Block ss' -> simplifyStatements $ ss' ++ ss
-      If Go.Ast.True ss' _ -> simplifyStatements $ ss' ++ ss
-      If Go.Ast.False _ ss' -> simplifyStatements $ ss' ++ ss
-      -- If e ss1 ss2 -> 
+-- simplifyStatements :: [Stmt] -> [Stmt]
+-- simplifyStatements = \case
+--   [] -> []
+--   s : ss -> case s of
+--     Skip -> simplifyStatements ss
+--     Return -> [Return]
+--     Break -> [Break]
+--     Block ss' -> simplifyStatements $ ss' ++ ss
+--     If CTrue ss' _ -> simplifyStatements $ ss' ++ ss
+--     If CFalse _ ss' -> simplifyStatements $ ss' ++ ss
+--     -- If e ss1 ss2 ->
 
-      Select [] Nothing -> [Select [] Nothing]
-      Select [] (Just ss') -> simplifyStatements $ ss' ++ ss
-      Go [] -> simplifyStatements ss
+--     Select [] Nothing -> [Select [] Nothing]
+--     Select [] (Just ss') -> simplifyStatements $ ss' ++ ss
+--     Go [] -> simplifyStatements ss
