3788fcdd403dacb9375cac36c47901f9ced7e11c
Vlad Saioc <vladsaioc10@gmail.com>
2023-08-08 13:42:04 +0200

    Collections removed from IR translation utilities.

--- src/Pipeline/IRTranslation/Utilities.hs
+++ src/Pipeline/IRTranslation/Utilities.hs
@@ -1,17 +1,15 @@
 module Pipeline.IRTranslation.Utilities where
 
-import Backend.Ast
+import Backend.Ast qualified as T
 import Backend.Utilities
 import Data.Map qualified as M
+import Utilities.Collection
 
 -- An alias for variable names (as strings) to clarify type definitions
 type 𝑋 = String
 
--- An alias for the map type constructor to increase lisibility of type definitions
-type a ↦ b = M.Map a b
-
 -- The type of type environments
-type 𝛴 = 𝑋 ↦ Type
+type 𝛴 = 𝑋 ↦ T.Type
 
 -- An alias for the type of process ids. Its purpose is to provide
 -- clarity to type definitions involving process ids.
@@ -19,21 +17,31 @@ type P = Int
 
 -- An alias for the type of program points. Its purpose is to provide
 -- clarity to type definitions involving program points.
-type P𝑛 = Int
+type 𝑁 = Int
 
 -- Bindings from program points to statements, encoding the semantics
 -- of the operation at the given point.
-type 𝛷 = P𝑛 ↦ Stmt
+type 𝛷 = 𝑁 ↦ T.Stmt
 
 -- Bindings from process ids to program points.
 type 𝛱 = P ↦ 𝛷
 
+-- Bindings from process ids to program point reachability conditions.
+type 𝛹 = P ↦ (𝑁 ↦ T.Exp)
+
 -- Program counter variable name. Produces the variable storing program
--- counters for each process. Given process id p,  the naming schema is
+-- counters for each process. Given process id p, the naming schema is
 -- (contents between braces are swapped with the variable contents):
 --  P{p}
 (⊲) :: P -> String
 (⊲) p = "P" ++ show p
+
+-- Program id to exit variable name. Produces the variable storing program
+-- exit points for each process. Given process id p, the naming schema is
+-- (contents between braces are swapped with the variable contents):
+--  X{p}
+(▽) :: P -> String
+(▽) p = "X" ++ show p
 
 -- Annotate process-local variable. Given process id p and name x,
 -- the naming schema is (contents between braces are swapped with
@@ -43,11 +51,15 @@ type 𝛱 = P ↦ 𝛷
 (%) p x = (p ⊲) ++ "'" ++ x
 
 -- Program id to program counter variable expression
-π :: P -> Exp
+π :: P -> T.Exp
 π p = ((p ⊲) @)
 
+-- Program id to exit variable expression.
+𝜒 :: P -> T.Exp
+𝜒 p = ((p ▽) @)
+
 -- Given a set of program points, produces the next available program point.
-(-|) :: 𝛷 -> Exp
+(-|) :: 𝛷 -> T.Exp
 (-|) 𝜙 = case M.toDescList 𝜙 of
   [] -> (0 #)
   (𝑛, _) : _ -> (𝑛 #)
@@ -59,10 +71,3 @@ equals = \case
   [a] -> Just a
   a' : a'' : as ->
     if a' == a'' then equals (a'' : as) else Nothing
-
--- Point-wise binary union between maps of maps of sets.
--- Given M1 and M2, it produces:
---
--- [ c ↦ [d ↦ M1(c)(d) ∪ M1(c)(d) | d ∈ dom(M1(c)) ∪ dom(M2(c))] | c ∈ dom(M1) ∪ dom(M2) ]
-(⊎) :: Ord a => Ord b => a ↦ (b ↦ [c]) -> a ↦ (b ↦ [c]) -> a ↦ (b ↦ [c])
-(⊎) = M.unionWith $ M.unionWith (++)
