fe980b055363151cbdce2a12821716fb8278d654
Georgian-Vlad Saioc <vsaioc@uber.com>
2023-11-09 11:46:14 +0100

    Parametricity profilers for Go and VirGo.

---
+++ src/Go/Profiler.hs
@@ -0,0 +1,85 @@
+module Go.Profiler (profileProgram) where
+
+import Go.Ast
+import Data.List qualified as L
+import Utilities.Position
+import Data.Monoid
+
+data Parametricity = Looping | Capacity | PathCondition
+  deriving (Read)
+
+instance Show Parametricity where
+  show = \case
+    Looping -> "loop parametric"
+    Capacity -> "capacity parametric"
+    PathCondition -> "path condition parametric"
+
+profileProgram :: Prog -> String
+profileProgram p =
+  let parametricity = getParametricity p
+   in if null parametricity then "Program is not parametric."
+      else "Program is " ++ L.intercalate " and " (map show parametricity)
+
+getParametricity :: Prog -> [Parametricity]
+getParametricity p =
+  let looping =  ([Looping | Any True == loopParametric p])
+      capping = ([Capacity | Any True == capParametric p])
+    in looping ++ capping
+
+traverseStmt :: Monoid a => Monoid b => (a -> Stmt -> a) -> (a -> Stmt -> b) -> a -> Pos Stmt -> b
+traverseStmt makecontext makeresult ctx (Pos p s) =
+  let tr = traverseStmt makecontext makeresult
+      aggregate ss = mconcat (map (tr (makecontext ctx s)) ss)
+      res = makeresult ctx
+  in case s of
+    Block ss -> aggregate ss
+    Go ss -> res s <> aggregate ss
+    While _ ss -> res s <> aggregate ss
+    For _ _ _ _ ss -> res s <> aggregate ss
+    If _ ss1 ss2 -> res s <> aggregate ss1 <> aggregate ss2
+    Select cs _ ->
+      let caseops = aggregate (map (Pos p . Atomic . (@^) . fst) cs)
+          casestmts = mconcat (map (aggregate . snd) cs)
+       in res s <> caseops <> casestmts
+    _ -> res s
+
+loopParametric :: Prog -> Any
+loopParametric (Prog ss) =
+  let makecontext looping = \case
+        For _ e1 e2 _ _ -> Any (parametricExp e1) <> Any (parametricExp e2)
+        _ -> looping
+      makeresult looping = \case
+        Atomic (Send _) -> looping
+        Atomic (Recv _) -> looping
+        _ -> Any False
+    in mconcat $ map (traverseStmt makecontext makeresult (Any False)) ss
+
+capParametric :: Prog -> Any
+capParametric (Prog ss) =
+  let makeresult _ = \case
+        Chan _ e -> Any (parametricExp e)
+        _ -> Any False
+    in mconcat $ map (traverseStmt mempty makeresult (Any False)) ss
+
+parametricExp :: Exp -> Bool
+parametricExp =
+  let bin e1 e2 = parametricExp e1 || parametricExp e2
+  in \case
+    CTrue -> False
+    CFalse -> False
+    CNum _ -> False
+    And e1 e2 -> bin e1 e2
+    Or e1 e2 -> bin e1 e2
+    Le e1 e2 -> bin e1 e2
+    Lt e1 e2 -> bin e1 e2
+    Ge e1 e2 -> bin e1 e2
+    Gt e1 e2 -> bin e1 e2
+    Eq e1 e2 -> bin e1 e2
+    Ne e1 e2 -> bin e1 e2
+    Not e -> parametricExp e
+    Neg e -> parametricExp e
+    Plus e1 e2 -> bin e1 e2
+    Minus e1 e2 -> bin e1 e2
+    Mult e1 e2 -> bin e1 e2
+    Div e1 e2 -> bin e1 e2
+    Var _ -> True
---
+++ src/IR/Profiler.hs
@@ -0,0 +1,78 @@
+module IR.Profiler (profileVirgo) where
+
+import IR.Ast
+import Data.List qualified as L
+import Data.Monoid
+
+data Parametricity = Looping | Capacity | PathCondition
+  deriving (Read)
+
+instance Show Parametricity where
+  show = \case
+    Looping -> "loop parametric"
+    Capacity -> "capacity parametric"
+    PathCondition -> "path condition parametric"
+
+profileVirgo :: 𝑃 -> String
+profileVirgo p =
+  let parametricity = getParametricity p
+   in if null parametricity then "Program is not parametric."
+      else "Program is " ++ L.intercalate " and " (map show parametricity)
+
+getParametricity :: 𝑃 -> [Parametricity]
+getParametricity p =
+  let looping =  ([Looping | Any True == loopParametric p])
+      capping = ([Capacity | Any True == capParametric p])
+    in looping ++ capping
+
+traverseStmt :: Monoid a => Monoid b => (a -> 𝑆 -> a) -> (a -> 𝑆 -> b) -> a -> 𝑆 -> b
+traverseStmt makecontext makeresult ctx s =
+  let tr = traverseStmt makecontext makeresult (makecontext ctx s)
+      res = makeresult ctx
+  in case s of
+    Seq s1 s2 -> res s <> tr s1 <> tr s2
+    Go s' -> res s <> tr s'
+    For _ _ _ os ->
+      let mkres c = makeresult c . Atomic
+       in res s <> mconcat (map (traverseOp mkres (makecontext ctx s)) os)
+    If _ ss1 ss2 -> res s <> tr ss1 <> tr ss2
+    _ -> res s
+
+traverseOp :: (a -> Op -> b) -> a -> Op -> b
+traverseOp makeresult = makeresult
+
+loopParametric :: 𝑃 -> Any
+loopParametric (𝑃 _ s) =
+  let makecontext looping = \case
+        For _ e1 e2 _ -> Any (parametricExp e1) <> Any (parametricExp e2)
+        _ -> looping
+      makeresult looping = \case
+        Atomic (Send _) -> looping
+        Atomic (Recv _) -> looping
+        _ -> Any False
+    in traverseStmt makecontext makeresult (Any False) s
+
+capParametric :: 𝑃 -> Any
+capParametric (𝑃 cs _) = mconcat (map (\(Chan _ e) -> Any $ parametricExp e) cs)
+
+parametricExp :: 𝐸 -> Bool
+parametricExp =
+  let bin e1 e2 = parametricExp e1 || parametricExp e2
+  in \case
+    Const _ -> False
+    BFalse -> False
+    BTrue -> False
+    e1 :& e2 -> bin e1 e2
+    e1 :| e2 -> bin e1 e2
+    e1 :== e2 -> bin e1 e2
+    e1 :!= e2 -> bin e1 e2
+    e1 :< e2 -> bin e1 e2
+    e1 :<= e2 -> bin e1 e2
+    e1 :> e2 -> bin e1 e2
+    e1 :>= e2 -> bin e1 e2
+    Not e -> parametricExp e
+    e1 :+ e2 -> bin e1 e2
+    e1 :- e2 -> bin e1 e2
+    e1 :* e2 -> bin e1 e2
+    e1 :/ e2 -> bin e1 e2
+    Var _ -> True
