9538d7b0eca2113d08287574fe40c0fe45b1cf3b
Georgian-Vlad Saioc <vsaioc@uber.com>
2023-12-11 13:54:05 +0200

    Clean up Dafny files in directories that will be analyzed.

--- recursive-run.sh
+++ recursive-run.sh
@@ -7,6 +7,9 @@ recursiveVerify () {
   do
     if [ -d $dir/$file ];
     then
+      rm -rf $dir/$file/*.dfy
+      rm -rf $dir/$file/*.res
+      rm -rf $dir/$file/*.dll
       recursiveVerify $dir/$file
     else
       if [[ $file = *"-ginger.pml" ]]; then
